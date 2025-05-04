aa1e74a3d85492a63bfacb81437ea875ea69e931
Igiiii780 <ignacyrembelski6@gmail.com>
2025-05-04 10:44:19 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,1 @@
-display.setStatusBar(display.HiddenStatusBar)
-
-local player = display.newCircle(160, 400, 20)
-player:setFillColor(0, 0.5, 1)
-
-local function move(event)
-    player.x = event.x
-    player.y = event.y
-end
-
-Runtime:addEventListener("touch", move)
+display.newText("Hello from Lua!", display.contentCenterX, display.contentCenterY, native.systemFont, 40)
